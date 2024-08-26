5e382b709bc09fbd694aa32d995960ec1966f9a6
catunix <harodonw@student.21-school.ru>
2024-08-26 03:10:47 +0300

    task3 ready

--- 3_task/fallen_students.py
+++ 3_task/fallen_students.py
@@ -1,22 +1,36 @@
 import unittest
 
+
 def fallen_student(A, Na, B, Nb):
     fallen_list = []
     
-
+    for i in range(0, Na):
+        if A[i] not in B[i]:
+            fallen_list.append(A[i])
+    
+    return fallen_list
 
 
 class TestFindSlidings(unittest.TestCase):
-    def test_empty_list(self):
+    def test_list_1(self):
         A = ['Иванов', 'Петров', 'Сидоров', 'Мариванна']
-        Na = len(A)
-        
         B = ['Зумеров', 'Петров', 'Скуффов', 'Скибиди', 'Эшкере']
+        Na = len(A)
         Nb = len(B)
 
         C = ['Иванов', 'Сидоров', 'Мариванна']
 
-        self.assertEqual([], fallen_student([]))
+        self.assertEqual(C, fallen_student(A, Na, B, Nb))
+
+    def test_list_2(self):
+        A = []
+        B = ['Зумеров', 'Петров', 'Скуффов', 'Скибиди', 'Эшкере']
+        Na = len(A)
+        Nb = len(B)
+
+        C = []
+
+        self.assertEqual(C, fallen_student(A, Na, B, Nb))
 
 if __name__ == '__main__':
     unittest.main()
